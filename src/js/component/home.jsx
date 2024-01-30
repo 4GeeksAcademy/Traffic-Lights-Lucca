96d6462e676288433a5771035e6270ab02c4bba1
Luccadz75 <149024268+Luccadz75@users.noreply.github.com>
2024-01-30 03:15:56 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import '../../styles/index.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TrafficLight = () => {
+  const [color, setColor] = useState('red');
+  const [hasPurple, setHasPurple] = useState(false);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleClick = (newColor) => {
+    if (newColor === 'purple') {
+      setHasPurple(true);
+    } else {
+      setColor(newColor);
+    }
+  };
+
+  const lightContainers = [
+    { color: 'red', glowClass: color === 'red' ? 'glow' : '' },
+    { color: 'yellow', glowClass: color === 'yellow' ? 'glow' : '' },
+    { color: 'green', glowClass: color === 'green' ? 'glow' : '' }
+   
+  ];
+
+  return (
+    <div className="traffic-light">
+      <div className="traffic-light-box">
+        <div className="tube"></div>
+        {lightContainers.map(container => (
+          <div key={container.color} className="light-container">
+            <div className={`light ${container.glowClass}`} style={{ backgroundColor: container.color }} onClick={() => handleClick(container.color)}></div>
+          </div>
+        ))}
+      </div>
+      <div className="button-container">
+        <button onClick={() => handleClick(color === 'red' ? 'yellow' : color === 'yellow' ? 'green' : 'red')}>Change Color</button>
+        
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+ReactDOM.render(<TrafficLight />, document.querySelector('#app'));
+
+export default TrafficLight;
+
+
+
+
+
+
+
+
+
+
+
